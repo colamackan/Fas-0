38f2a8215112f4ce4d64746d5d52f773f55f705d
Marcus Froling <mafr0930@celsius.it.uu.se>
2014-10-09 14:59:56 +0200

    new file:   travelplanner.c

---
+++ travelplanner.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct node{
+  char *name;
+  char *line;
+  int *traveltime;
+  struct node *next;
+}*Node;
+
+
+void find_matching_route(){
+}
+
+void possible_routes(){
+}
+
+void update_network(){
+}
+
+void delete_route(){
+}
+
+void insert_route(){
+} 
+
+Node delete_busstop(){
+}
+
+Node insert_busstop(){
+}
+
+void readline(FILE *busnetwork){
+
+}
+
+Node make_network (char *network){
+  FILE *busnetwork = fopen("~/IOOPM/natverk.txt","r");
+  Node busstop = malloc(sizeof(struct node));
+  
+}
+
+
+int main(){
+  FILE *start = fopen("~/IOOPM/start.txt","r");
+  return 0;
+}
