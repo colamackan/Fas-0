79a524094920aebda716425ec50ebf6686c00cff
Marcus Froling <mafr0930@linne.it.uu.se>
2014-10-15 13:09:28 +0200

    a

--- travelplanner.c
+++ travelplanner.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+
+
 /* Startnod sätts till 0, nästa nod får värdet föregående nod + edge */
 
 typedef struct route{
@@ -42,23 +44,25 @@ Node insert_busstop(){
 }
 */
 
-int readline(char *dest, int n, FILE *source){
+
+
+char readline(char *dest, int n, FILE *source){
   fgets(dest, n, source);
   int len = strlen(dest);
   if(dest[len-1] == '\n')
     dest[len-1] = '\0';
-  return len;
+  return *dest;
 }
 
 
 Node make_network (char *network){
   char *list = NULL;
   FILE *busnetwork = fopen(network,"r");
+  list = readline(list, 128, busnetwork);
   Node busstop = malloc(sizeof(struct node));
-  readline(list, 128, busnetwork);
   for(int i = 0; i != '\0'; i++){
-    if (isalpha(&busstop[i])){;
-      list = (isalpha(&busstop[i]));
+    if (isalpha(list[i])){;
+      list = (isalpha(list[i]));
       while (list != NULL){
 	if (strcmp(list, busstop->name) == 0){
 	  busstop->next = malloc(strlen(list)+1);
@@ -76,8 +80,11 @@ Node make_network (char *network){
   return busstop;
 }
 
+
+
 int main(int argc, char *argv[]){
   make_network(argv[1]);
   return 0;
 }
+ 
  
